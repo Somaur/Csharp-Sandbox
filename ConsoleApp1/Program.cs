177669b56932abcbfa8d54d0ad91ff96e70ca910
Somaur <1376129425@qq.com>
2022-09-17 23:32:28 +0800

    添加项目文件。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,9 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        int number = 1719820;
+        Console.WriteLine(Convert.ToString(number, 2).PadLeft(32, '0'));
+    }
+}
