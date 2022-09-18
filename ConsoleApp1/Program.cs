3fa6e7e15eb548aa3f4cc07d15be2ce60a0e1705
Somaur <1376129425@qq.com>
2022-09-18 12:07:09 +0800

    void Convert.ToString(int value, int toBase)

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,8 +4,10 @@
     {
         Console.WriteLine("Hello, World!");
         int number = 1719820;
+        // C# 的库函数 能够将数字转为二进制字符串
+        // PadLeft 是字符串处理函数 用于左边添加0
         Console.WriteLine(Convert.ToString(number, 2).PadLeft(32, '0'));
-        // 这是一个注释
-        // if it works...
+        // 虽然第二个参数填进制，但是只有 2 8 10 16 进制可用，其他输入会报异常
+        // Console.WriteLine(Convert.ToString(number, 4).PadLeft(32, '0')); // ArgumentException
     }
 }
